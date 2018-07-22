135a5c978dc5b3f83cc5aa0d1d994e913e312497
Gao <gt70@duke.edu>
2018-07-22 19:56:39 -0400

    allow no shared memory

--- pyLib/io/__init__.py
+++ pyLib/io/__init__.py
@@ -3,4 +3,7 @@ from .argument import getOnOffArgs as getArgs
 from .fileio import ddctParse, ddctSave, getJson, dumpJson, assignModule, assignModuleByJson, getLogPath
 from .printio import Mute
 from .stringio import getNumber, numRe, joinNumber
-import sharedmemory
+try:
+    import sharedmemory
+except:
+    pass
