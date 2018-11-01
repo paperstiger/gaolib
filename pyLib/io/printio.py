3f0bc46f150426717bec9cd51e406808d1a14804
Gao Tang <gao.tang@duke.edu>
2018-10-31 21:22:04 -0400

    change Mute so it mutes external library, too

--- pyLib/io/printio.py
+++ pyLib/io/printio.py
@@ -12,32 +12,21 @@ printio.py
 Control print
 """
 import sys
+import os
+from cStringIO import StringIO
 
 
 class Mute(list):
     """Capture std.output."""
-    def __init__(self, mute=True, record=False):
+    def __init__(self, mute=True):
         self.mute = mute
-        self.record = record
-        if self.record:
-            self.mute = True
 
     def __enter__(self):
         if self.mute:
-            self._stdout = sys.stdout
-            if not self.record:
-                sys.stdout = open('/dev/null')
-            else:
-                sys.stdout = self._stringio = StringIO()
+            devnull = open(os.devnull, 'w')
+            self.old_std = os.dup(sys.stdout.fileno())
+            os.dup2(devnull.fileno(), 1)
 
     def __exit__(self, *args):
         if self.mute:
-            if self.record:
-                del self._stringio    # free up some memory
-            sys.stdout = self._stdout
-
-    def getOutput(self):
-        if not self.record:
-            return None
-        else:
-            return self._stringio.getvalue()
+            os.dup2(self.old_std, 1)
